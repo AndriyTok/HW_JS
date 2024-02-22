90eaf30b42c20980b18353da11dd074e40656883
AndriyTok <andriy.tokovyy@gmail.com>
2024-02-22 17:06:19 +0200

    First task done✔️

---
+++ HW_1/task1.js
@@ -0,0 +1,28 @@
+//task 1
+let a = "hello";
+let b= "owu";
+let c="com";
+let d="ua";
+let num1=1;
+let num2=10;
+let num3=-999;
+let num4=123;
+let num5=3.14;
+let num6=2.7;
+let num7=16;
+let bool1=true;
+let bool2=false;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
+console.log(num7);
+console.log(bool1);
+console.log(bool2);
+
