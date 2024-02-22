984c5f3d41a49eeee1b967e92749e2e110f07076
AndriyTok <andriy.tokovyy@gmail.com>
2024-02-22 18:01:26 +0200

    task 2 done✔️

--- HW_1/task2.js
+++ HW_1/task2.js
@@ -3,3 +3,9 @@ console.log('----------------------Task2-------------------------');
 let firstName=`Andriy`;
 let middleName=`Tokovyy`;
 let lastName=`Yuriyovych`;
+//first way of concatenation
+let person=`${firstName} ${middleName} ${lastName}`
+//second way of concatenation
+// let person2=firstName+' '+middleName+' '+lastName+' ';
+console.log(person);
+// console.log(person2);
