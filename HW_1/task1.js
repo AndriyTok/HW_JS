047eadaee0a23ab5ba78b885eba99ec9f7c38305
AndriyTok <andriy.tokovyy@gmail.com>
2024-02-22 17:25:04 +0200

    task 3 done✔️

--- HW_1/task1.js
+++ HW_1/task1.js
@@ -1,8 +1,8 @@
 //task 1
-let a = "hello";
-let b= "owu";
-let c="com";
-let d="ua";
+let st1 = "hello";
+let st2= "owu";
+let st3="com";
+let st4="ua";
 let num1=1;
 let num2=10;
 let num3=-999;
@@ -12,10 +12,10 @@ let num5=2.7;
 let num6=16;
 let bool1=true;
 let bool2=false;
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
+console.log(st1);
+console.log(st2);
+console.log(st3);
+console.log(st4);
 console.log(num1);
 console.log(num2);
 console.log(num3);
---
+++ HW_1/task3.js
@@ -0,0 +1,5 @@
+//task3
+let a = 100; let b = '100'; let c = true;
+console.log(a, typeof a);
+console.log(b, typeof b);
+console.log(c, typeof c);
