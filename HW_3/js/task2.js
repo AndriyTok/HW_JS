4af55acaa6a3e1d91bce5dfcc1552dd926515a71
AndriyTok <andriy.tokovyy@gmail.com>
2024-02-27 21:34:48 +0200

    HW3 task 2 done✔️

---
+++ HW_3/js/task2.js
@@ -0,0 +1,25 @@
+// - Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону
+// Масив:
+//
+//     let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+//
+// ШАБЛОН:
+//     <ul>
+//         <li>ITEM OF ARRAY</li>
+//         <!--
+//             і тд інші об'єкти масиву
+//              ...
+//              ...
+//              ...
+//         -->
+//     </ul>
+//
+// замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
+
+let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+for (const Item of listOfItems) {
+    document.write(`<div><ul>
+<li>${Item}</li>
+</ul></div>`)
+}
